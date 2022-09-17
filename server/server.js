c9d401844fb73a48b2e78644ac02d3909dd6ddf8
lukaeric14 <lukaericnu@gmail.com>
2022-09-17 00:05:25 -0500

    make deployable

--- server/server.js
+++ server/server.js
@@ -16,12 +16,13 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.use("/api", appointmentRoute);
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+app.get("/bookings", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
